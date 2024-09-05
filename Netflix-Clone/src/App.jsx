bc54b4fbe25b408a21388b2c7742449a224124d4
Berhan <berhan92gmail.com>
2024-09-05 14:38:31 -0400

    header and footer added

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
 import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
-  return <></>;
+  return <>
+  <Home/>
+  </>;
 }
 
 export default App;
