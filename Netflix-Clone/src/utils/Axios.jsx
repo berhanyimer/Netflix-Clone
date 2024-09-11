1472a235ebdebebacb3ef7c635a915e1e97d7246
Berhan <berhan92gmail.com>
2024-09-11 01:10:02 -0400

    axios import fixed

--- Netflix-Clone/src/utils/Axios.jsx
+++ Netflix-Clone/src/utils/Axios.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 // Create an instance of axios with a custom configuration
 const instance = axios.create({
+  // TMDb base URL
   baseURL: "https://api.themoviedb.org/3",
 });
 
